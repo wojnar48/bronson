8b1ade718bd637fdc6098b100140160f76499e7c
Szymon Wojnar <wojnar48@gmail.com>
2017-05-09 19:16:41 -0400

    Modify db_connection file to accept wider range of db files

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -1,10 +1,9 @@
 require 'sqlite3'
-require 'byebug'
 
 PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
 ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
-CATS_SQL_FILE = File.join(ROOT_FOLDER, 'cats.sql')
-CATS_DB_FILE = File.join(ROOT_FOLDER, 'cats.db')
+SQL_FILE = File.join(ROOT_FOLDER, Dir.glob('*.sql').first)
+DB_FILE = File.join(ROOT_FOLDER, Dir.glob('*.db').first)
 
 class DBConnection
   def self.open(db_file_name)
@@ -17,12 +16,12 @@ class DBConnection
 
   def self.reset
     commands = [
-      "rm '#{CATS_DB_FILE}'",
-      "cat '#{CATS_SQL_FILE}' | sqlite3 '#{CATS_DB_FILE}'"
+      "rm '#{DB_FILE}'",
+      "cat '#{SQL_FILE}' | sqlite3 '#{DB_FILE}'"
     ]
 
     commands.each { |command| `#{command}` }
-    DBConnection.open(CATS_DB_FILE)
+    DBConnection.open(DB_FILE)
   end
 
   def self.instance
