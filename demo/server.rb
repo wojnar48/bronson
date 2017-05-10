2eb74291c212cc9ba3425bd18725cb8870ba3925
Szymon Wojnar <wojnar48@gmail.com>
2017-05-10 15:27:03 -0400

    Add ascii art demo

--- demo/server.rb
+++ demo/server.rb
@@ -4,50 +4,27 @@ require_relative '../lib/router'
 require_relative '../lib/sql_object'
 require 'byebug'
 
-class Cat < SQLObject
+class Image < SQLObject
   self.finalize!
 end
 
-class Human < SQLObject
-  self.table_name = 'humans'
+class User < SQLObject
+  self.table_name = 'images'
   self.finalize!
 end
 
-class CatsController < ControllerBase
-  def create
-    @cat = Cat.new
-    @cat.name = params['name']
-
-    Human.table_name = 'humans'
-    @owner = Human.new
-    @owner.fname = params['fname']
-    @owner.lname = params['lname']
-    @owner.save
-
-    @cat.owner_id = @owner.id
-    @cat.save
-
-    self.redirect_to('cats')
-  end
-
-  def new
-    @cat = Cat.new
-    render('new')
-  end
-
+class ImagesController < ControllerBase
   def index
-    # debugger
-    @cats = Cat.all
+    @images = Image.all
     render('index')
   end
 end
 
 router = Router.new
 router.draw do
-  get Regexp.new("^/cats$"), CatsController, :index
-  get Regexp.new("^/cats/new$"), CatsController, :new
-  post Regexp.new("^/cats$"), CatsController, :create
-  # get Regexp.new("^/cats/(?<cat_id>\\d+)/statuses$"), StatusesController, :index
+  get Regexp.new("^/images$"), ImagesController, :index
+  # get Regexp.new("^/cats/new$"), CatsController, :new
+  # post Regexp.new("^/cats$"), CatsController, :create
 end
 
 app = Proc.new do |env|
